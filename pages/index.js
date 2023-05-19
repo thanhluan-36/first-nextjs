b86d387462036bc1c05ab1e073dacd60b8257d9d
thanhluan-36 <vuvuthanhluan@gmail.com>
2023-05-19 23:49:46 +0700

    commit thay doi head

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
-        <h2>Tôi tên là VŨ THÀNH LUÂN</h2>
+        <h2>Tôi tên là VŨ THÀNH LUÂN @@</h2>
 
         <p className={styles.description}>
           Get started by editing <code>pages/index.js</code>
